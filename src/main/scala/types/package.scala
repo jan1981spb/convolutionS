5ddab65de16999278b5ec37babe3ef742be19ca6
Вячеславович Гришин Кирилл <k.grishin@voskhod.ru>
2024-05-10 11:22:55 +0300

    core and add edges

--- src/main/scala/types/package.scala
+++ src/main/scala/types/package.scala
@@ -1,7 +1,33 @@
 package object types {
 
   type Batches = Map[(Int, Int), Seq[Seq[Int]]]
-  type OutputPartition = Seq[Seq[Double]]
-  type Output = OutputPartition
+  type Input = Seq[Seq[Int]]
+  type SupplementedInput = Input
+  type Output = Input
+
+
+  final case class InputDataFrame(input: Input)
+  object InputDataFrame {
+    def apply(input: Input): Either[String, InputDataFrame] = input match {
+      case in if (in.size == in.head.size) => Right(new InputDataFrame(in))
+      case _ => Left("width and height of input dataframe are not equal")
+    }
+  }
+
+  final case class SupplementedDataFrame(input: Input)
+  object SupplementedDataFrame {
+    def apply(input: Input): Either[String, SupplementedDataFrame] = input match {
+      case in if (in.size == in.head.size) => Right(new SupplementedDataFrame(in))
+      case _ => Left("width and height of supplemented dataframe are not equal")
+    }
+  }
+
+  final case class Core(size: Int)
+  object Core {
+    def apply(size: Int): Option[Core] = size match {
+      case size if size % 2 != 0 => Some(new Core(size))
+      case _ => None
+    }
+  }
 
 }
---
+++ src/test/scala/DataFrameTest.scala
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test
+import service.DataFrameSource
+
+class DataFrameTest {
+
+  @Test
+  def generateDataFrameWithIncreaseNumbersInEachRowSuccess(): Unit = {
+      val df = DataFrameSource.generateDataFrameWithIncreaseNumbersInEachRow(9, 9)
+      assert(df.isRight)
+  }
+
+  @Test
+  def generateDataFrameWithIncreaseNumbersInEachRowFailed(): Unit = {
+    val df = DataFrameSource.generateDataFrameWithIncreaseNumbersInEachRow(9, 8)
+    assert(df.isLeft)
+  }
+
+}
